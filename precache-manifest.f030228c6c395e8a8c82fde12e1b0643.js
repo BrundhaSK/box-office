4404f0c088f97424da2c351801024f5f9f3cc8f1
brundhask <brundhasubramanian@gmail.com>
2021-08-07 16:36:05 -0700

    Updates

---
+++ precache-manifest.f030228c6c395e8a8c82fde12e1b0643.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6759cef324a700a3dcf701b7b43bece",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "9c0f09c2c1049f5973d6",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "b1442ccdcdfa61db6ad0",
+    "url": "/box-office/static/js/2.7927cd51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.7927cd51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c0f09c2c1049f5973d6",
+    "url": "/box-office/static/js/main.add554e0.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
